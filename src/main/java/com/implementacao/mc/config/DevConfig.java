9d4c33849a61b31e32604f901b9eeb9c641ca1c6
GilbertoSantanaCosta <g.s.d.c@hotmail.com>
2021-08-29 17:52:53 -0300

    implementação SmtpEmailService com servidor do google

--- src/main/java/com/implementacao/mc/config/DevConfig.java
+++ src/main/java/com/implementacao/mc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.implementacao.mc.service.DBservice;
+import com.implementacao.mc.service.EmailService;
+import com.implementacao.mc.service.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -30,5 +32,10 @@ public class DevConfig {
 		
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 }
--- src/main/java/com/implementacao/mc/service/PedidoService.java
+++ src/main/java/com/implementacao/mc/service/PedidoService.java
@@ -5,14 +5,17 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.implementacao.mc.domain.Cliente;
 import com.implementacao.mc.domain.ItemPedido;
 import com.implementacao.mc.domain.PagamentoComBoleto;
 import com.implementacao.mc.domain.Pedido;
 import com.implementacao.mc.domain.enums.EstadoPagamento;
 import com.implementacao.mc.exceptions.ObjectNotFoundException;
+import com.implementacao.mc.repositories.ClienteRepository;
 import com.implementacao.mc.repositories.ItemPedidoRepository;
 import com.implementacao.mc.repositories.PagamentoRepository;
 import com.implementacao.mc.repositories.PedidoRepository;
+import com.implementacao.mc.repositories.ProdutoRepository;
 
 
 @Service
@@ -34,6 +37,12 @@ public class PedidoService {
 	private ProdutoService produtoService;
 	
 	@Autowired
+	private ClienteService clienteService;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
+	@Autowired
 	private EmailService emailService;
 	
 	public Pedido find(Integer id ) {
@@ -49,6 +58,7 @@ public class PedidoService {
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
 		if (obj.getPagamento() instanceof PagamentoComBoleto) {
@@ -59,10 +69,12 @@ public class PedidoService {
 		pagamentoRepository.save(obj.getPagamento());
 		for (ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
+			ip.setProduto(produtoRepository.findById(ip.getProduto().getId()).get());
 			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
 			ip.setPedido(obj);
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
+		System.out.println(obj);
 		emailService.sendOrderConfirmationEmail(obj);
 		return obj;
 	}
