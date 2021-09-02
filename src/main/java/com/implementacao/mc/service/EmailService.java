4bc7cf72cd31751640b555c6089696d6296ee547
GilbertoSantanaCosta <g.s.d.c@hotmail.com>
2021-09-02 20:22:46 -0300

    email-html

--- src/main/java/com/implementacao/mc/service/EmailService.java
+++ src/main/java/com/implementacao/mc/service/EmailService.java
@@ -1,5 +1,7 @@
 package com.implementacao.mc.service;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.implementacao.mc.domain.Pedido;
@@ -9,4 +11,9 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
+	
 }
